693f83e23221a1f14c6c5493c1b9d754f081bdc6
niyongqiang1 <xiao1012451454@163.com>
2024-05-28 21:04:13 +0800

    这是0528第一次提交的python代码

---
+++ p.py
@@ -0,0 +1,11 @@
+# 画五角星
+'''
+import turtle
+turtle.goto(100,0)
+for i in range(20):
+    turtle.left(80)
+    turtle.fd(100)
+    turtle.left(135)
+    turtle.forward(105)
+'''
+print("这是0528第一次提交的python代码")
